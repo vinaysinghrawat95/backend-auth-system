15f6f240527ec3f59203e6bcfbfd7b5a97991960
Vinay Singh Rawat <vinaysinghrawat505@gmail.com>
2026-04-06 19:19:31 +0530

    Enhance backend response structure with global exception handling and JWT response DTO

--- auth-system/src/main/java/com/vinay/auth_system/dto/AuthResponse.java
+++ auth-system/src/main/java/com/vinay/auth_system/dto/AuthResponse.java
@@ -1,10 +1,16 @@
 package com.vinay.auth_system.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AuthResponse {
+
     private String jwtToken;
+    private String type;
+
+    public AuthResponse(String jwtToken){
+        this.jwtToken = jwtToken;
+        this.type = "success";
+    }
+
 }
--- auth-system/src/main/java/com/vinay/auth_system/exception/GlobalExceptionHandler.java
+++ auth-system/src/main/java/com/vinay/auth_system/exception/GlobalExceptionHandler.java
@@ -2,18 +2,33 @@ package com.vinay.auth_system.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<Map<String, String>> handleBadCredential(BadCredentialsException badCredentialsException){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("message", "Invalid email or password",
+                             "type", "error"));
+    }
+
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntime(RuntimeException ex){
-        return ResponseEntity.status(HttpStatus.CONFLICT)
-                .body(ex.getMessage());
+    public ResponseEntity<Map<String, String>> handleRuntime(RuntimeException runtimeException){
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("message", runtimeException.getMessage()));
     }
 }
+
+
+
 
 
 
